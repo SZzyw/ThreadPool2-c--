8fa90d8c385aeccdd0f18e1a0bbb80654a053802
SZzyw <zyw.4.1@qq.com>
2026-03-27 10:44:40 +0800

    解决了管理线程缩容时直接销毁 isnull。 把 exitvalue 的检查放进了等待循环里，销毁的进程没有任务。

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,45 +1,50 @@
 #include "ThreadPool.h"
 
-ThreadPool::ThreadPool(int minvalue,int maxvalue):
-livenum(minvalue),busynum(0),minvalue(minvalue),maxvalue(maxvalue),exitvalue(0),shutdown(true){
-    queue=new TaskQueue();
+ThreadPool::ThreadPool(int minvalue, int maxvalue) : livenum(minvalue), busynum(0), minvalue(minvalue), maxvalue(maxvalue), exitvalue(0), shutdown(true)
+{
+    queue = new TaskQueue();
 
-    pthread_mutex_init(&poolMutex,nullptr);
-    pthread_mutex_init(&busyMutex,nullptr);
-    pthread_cond_init(&isnull,nullptr);
+    pthread_mutex_init(&poolMutex, nullptr);
+    pthread_mutex_init(&busyMutex, nullptr);
+    pthread_cond_init(&isnull, nullptr);
 
-    workerId.resize(maxvalue,0);
+    workerId.resize(maxvalue, 0);
 
-    pthread_create(&managerId,nullptr,manager,this);
-    for(int i=0;i<minvalue;i++)
-        pthread_create(&workerId[i],nullptr,worker,this);
+    pthread_create(&managerId, nullptr, manager, this);
+    for (int i = 0; i < minvalue; i++)
+        pthread_create(&workerId[i], nullptr, worker, this);
 }
 
-void* ThreadPool::worker(void *arg){
-    ThreadPool* pool=(ThreadPool*) arg;
+void *ThreadPool::worker(void *arg)
+{
+    ThreadPool *pool = (ThreadPool *)arg;
 
-    while(true){
+    while (true)
+    {
         pthread_mutex_lock(&pool->poolMutex);
-        while(pool->queue->taskSize()==0&&pool->shutdown){
-            pthread_cond_wait(&pool->isnull,&pool->poolMutex);
-            if(pool->exitvalue>0){
+        while (pool->queue->taskSize() == 0 && pool->shutdown)
+        {
+            if (pool->exitvalue > 0)
+            {
                 pool->exitvalue--;
                 pool->livenum--;
                 pthread_mutex_unlock(&pool->poolMutex);
                 pool->threadExit();
             }
+            pthread_cond_wait(&pool->isnull, &pool->poolMutex);
         }
-        if(pool->shutdown==false){
+        if (pool->shutdown == false)
+        {
             pthread_mutex_unlock(&pool->poolMutex);
             pool->threadExit();
         }
-        Task task=pool->queue->taskGet();
+        Task task = pool->queue->taskGet();
         pthread_mutex_unlock(&pool->poolMutex);
 
         pthread_mutex_lock(&pool->busyMutex);
         pool->busynum++;
         pthread_mutex_unlock(&pool->busyMutex);
-        
+
         task.function(task.arg);
 
         pthread_mutex_lock(&pool->busyMutex);
@@ -48,35 +53,43 @@ void* ThreadPool::worker(void *arg){
     }
 }
 
-void ThreadPool::threadExit(){
-    for(int i=0;i<this->maxvalue;i++){
-        if(this->workerId[i]==pthread_self()){
-            this->workerId[i]=0;
+void ThreadPool::threadExit()
+{
+    for (int i = 0; i < this->maxvalue; i++)
+    {
+        if (this->workerId[i] == pthread_self())
+        {
+            this->workerId[i] = 0;
             break;
         }
     }
     pthread_exit(nullptr);
 }
 
-void *ThreadPool::manager(void *arg){
-    ThreadPool* pool=(ThreadPool*) arg;
-    while(pool->shutdown){
+void *ThreadPool::manager(void *arg)
+{
+    ThreadPool *pool = (ThreadPool *)arg;
+    while (pool->shutdown)
+    {
         sleep(3);
 
         pthread_mutex_lock(&pool->poolMutex);
-        int livenum=pool->livenum;
+        int livenum = pool->livenum;
         pthread_mutex_unlock(&pool->poolMutex);
         pthread_mutex_lock(&pool->busyMutex);
-        int busynum=pool->busynum;
+        int busynum = pool->busynum;
         pthread_mutex_unlock(&pool->busyMutex);
-        int maxvalue=pool->maxvalue;
-        int minvalue=pool->minvalue;
+        int maxvalue = pool->maxvalue;
+        int minvalue = pool->minvalue;
 
-        if(pool->queue->taskSize()>livenum&&livenum<maxvalue){
+        if (pool->queue->taskSize() > livenum && livenum < maxvalue)
+        {
             pthread_mutex_lock(&pool->poolMutex);
-            for(int i=0;i<maxvalue&&pool->shutdown;i++){
-                if(pool->workerId[i]==0){
-                    pthread_create(&pool->workerId[i],nullptr,worker,pool);
+            for (int i = 0; i < maxvalue && pool->shutdown; i++)
+            {
+                if (pool->workerId[i] == 0)
+                {
+                    pthread_create(&pool->workerId[i], nullptr, worker, pool);
                     pool->livenum++;
                     break;
                 }
@@ -84,45 +97,52 @@ void *ThreadPool::manager(void *arg){
             pthread_mutex_unlock(&pool->poolMutex);
         }
 
-        if(busynum*2<=livenum&&livenum>minvalue){
+        if (busynum * 2 <= livenum && livenum > minvalue)
+        {
             pthread_mutex_lock(&pool->poolMutex);
-            pool->exitvalue=1;
+            pool->exitvalue = 1;
             pthread_mutex_unlock(&pool->poolMutex);
-            pthread_cond_destroy(&pool->isnull);
+            pthread_cond_broadcast(&pool->isnull);
         }
     }
     pthread_exit(nullptr);
 }
 
-void ThreadPool::addTask(Task task){
-    if(this->shutdown==false){
+void ThreadPool::addTask(Task task)
+{
+    if (this->shutdown == false)
+    {
         return;
     }
     this->queue->taskAdd(task);
     pthread_cond_signal(&this->isnull);
 }
 
-int ThreadPool::getLivenum(){
+int ThreadPool::getLivenum()
+{
     pthread_mutex_lock(&this->poolMutex);
-    int livenum=this->livenum;
+    int livenum = this->livenum;
     pthread_mutex_unlock(&this->poolMutex);
     return livenum;
 }
 
-int ThreadPool::getBugynum(){
+int ThreadPool::getBugynum()
+{
     pthread_mutex_lock(&this->busyMutex);
-    int busynum=this->busynum;
+    int busynum = this->busynum;
     pthread_mutex_unlock(&this->busyMutex);
     return busynum;
 }
 
-ThreadPool::~ThreadPool(){
-    this->shutdown=false;
-    for(int i=0;i<this->maxvalue+10;i++){
-        pthread_cond_destroy(&isnull);
+ThreadPool::~ThreadPool()
+{
+    this->shutdown = false;
+    for (int i = 0; i < this->maxvalue + 10; i++)
+    {
+        pthread_cond_broadcast(&isnull);
     }
-    delete(this->queue);
-    this->queue=nullptr;
+    delete (this->queue);
+    this->queue = nullptr;
 
     pthread_mutex_destroy(&poolMutex);
     pthread_mutex_destroy(&busyMutex);
